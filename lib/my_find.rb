1cbd8f9880e7d4c7d02dcae77da3b0871b3963bc
Lisa Walters <campi_l@fortlewis.edu>
2020-08-26 20:34:18 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,11 @@ def my_find(collection)
 
   i = 0
   while i < collection.length
-    return collection.[i]
+    return
+    collection.[i]
     if
     yield(collection[i])
 
     i += 1
   end
-
 end
